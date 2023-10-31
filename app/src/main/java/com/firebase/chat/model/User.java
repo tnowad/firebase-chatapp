f96d068f290f339367c20938995b67b4fc561ca0
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-31 19:21:34 +0700

    feat(model): add attribute user model

--- app/src/main/java/com/firebase/chat/model/User.java
+++ app/src/main/java/com/firebase/chat/model/User.java
@@ -3,9 +3,10 @@ package com.firebase.chat.model;
 import java.util.List;
 
 public class User {
-    private String gmail;
     private String id;
+    private String email;
     private String fullName;
+    private String address;
     private String bio;
     private Boolean gender;
 }
