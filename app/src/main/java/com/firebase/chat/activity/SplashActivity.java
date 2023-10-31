1b84c90cd92906c4b777b26c2e4fe61df4d0830f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-31 14:16:02 +0700

    feat(splash): override start app with firebase

--- app/src/main/java/com/firebase/chat/activity/SplashActivity.java
+++ app/src/main/java/com/firebase/chat/activity/SplashActivity.java
@@ -6,14 +6,34 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.chat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startActivity(new Intent(this, LoginActivity.class));
+        setContentView(R.layout.activity_splash);
 
-        finish();
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser == null){
+            Intent loginIntent = new Intent(SplashActivity.this,LoginActivity.class);
+            startActivity(loginIntent
+            );
+            finish();
+        }
+        else{
+            Intent chatIntent = new Intent(SplashActivity.this,ChatActivity.class);
+            startActivity(chatIntent);
+            finish();
+        }
     }
 }
