eeebd4004a44c6f1f0eb283374dbe3bedde29ad1
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-04 22:17:47 +0700

    feat(message-service): add get current user for message service

--- app/src/main/java/com/firebase/chat/services/MessageService.java
+++ app/src/main/java/com/firebase/chat/services/MessageService.java
@@ -11,7 +11,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -22,14 +22,13 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MessageService extends BaseService {
-    private UserService userService = new UserService();
+    private final UserService userService = new UserService();
+    private final FirebaseUser firebaseUser = AuthService.getInstance().getCurrentUser();
 
     public MessageService() {
         super("messages");
