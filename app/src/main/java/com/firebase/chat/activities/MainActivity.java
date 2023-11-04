d07c268d5c82976428f4616ca2df16e2ccf0250f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-04 16:31:12 +0700

    refactor(main-activity): change from new class to lamda expression

--- app/src/main/java/com/firebase/chat/activities/MainActivity.java
+++ app/src/main/java/com/firebase/chat/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package com.firebase.chat.activities;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.viewpager2.widget.ViewPager2;
@@ -12,7 +10,6 @@ import com.firebase.chat.R;
 import com.firebase.chat.adapters.AdapterViewPager;
 import com.firebase.chat.databinding.ActivityMainBinding;
 import com.firebase.chat.viewmodels.MainViewModel;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         binding.setMainViewModel(mainViewModel);
 
         setupViewPager(binding);
+
         setupBottomNavigationBar(binding);
     }
 
@@ -35,10 +33,12 @@ public class MainActivity extends AppCompatActivity {
         AdapterViewPager adapterViewPager = new AdapterViewPager(this);
         ViewPager2 viewPager = binding.MainActivityViewPager2;
         viewPager.setAdapter(adapterViewPager);
+
         viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
+
                 switch (position) {
                     case 0:
                         binding.MainActivityBottomNav.setSelectedItemId(R.id.BottomNavMenu_Item_Message);
@@ -55,22 +55,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupBottomNavigationBar(ActivityMainBinding binding) {
-        binding.MainActivityBottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int messageItemId = R.id.BottomNavMenu_Item_Message;
-                int profileItemId = R.id.BottomNavMenu_Item_Profile;
-                int settingItemId = R.id.BottomNavMenu_Item_Setting;
+        binding.MainActivityBottomNav.setOnItemSelectedListener(item -> {
+            int messageItemId = R.id.BottomNavMenu_Item_Message;
+            int profileItemId = R.id.BottomNavMenu_Item_Profile;
+            int settingItemId = R.id.BottomNavMenu_Item_Setting;
 
-                if (item.getItemId() == messageItemId) {
-                    binding.MainActivityViewPager2.setCurrentItem(0);
-                } else if (item.getItemId() == profileItemId) {
-                    binding.MainActivityViewPager2.setCurrentItem(1);
-                } else if (item.getItemId() == settingItemId) {
-                    binding.MainActivityViewPager2.setCurrentItem(2);
-                }
-                return true;
+            if (item.getItemId() == messageItemId) {
+                binding.MainActivityViewPager2.setCurrentItem(0);
+            } else if (item.getItemId() == profileItemId) {
+                binding.MainActivityViewPager2.setCurrentItem(1);
+            } else if (item.getItemId() == settingItemId) {
+                binding.MainActivityViewPager2.setCurrentItem(2);
             }
+            return true;
         });
     }
 }
