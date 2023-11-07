3569f3f6b76432381e51fe4dc1aa4ae85d287365
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-08 00:13:11 +0700

    feat(friend-service): add send friend request

--- app/src/main/java/com/firebase/chat/services/FriendService.java
+++ app/src/main/java/com/firebase/chat/services/FriendService.java
@@ -1,10 +1,43 @@
 package com.firebase.chat.services;
 
-public class FriendService extends BaseService {
+import com.firebase.chat.models.Friend;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class FriendService {
+
+    private static FriendService instance;
+    FirebaseFirestore firestore;
+    CollectionReference friendsRef;
 
     public FriendService() {
-        super("friends");
+        firestore = FirebaseFirestore.getInstance();
+        friendsRef = firestore.collection("friends");
     }
 
-    
+    public static FriendService getInstance() {
+        if (instance == null) {
+            instance = new FriendService();
+        }
+        return instance;
+    }
+
+    public Task<DocumentReference> sendFriendRequest(String senderId, String receiverId) {
+        Friend friend = new Friend(senderId, receiverId, "pending");
+        TaskCompletionSource<DocumentReference> taskCompletionSource = new TaskCompletionSource<>();
+
+        friendsRef.add(friend)
+                .addOnSuccessListener(documentReference -> {
+                    taskCompletionSource.setResult(documentReference);
+                })
+                .addOnFailureListener(e -> {
+                    taskCompletionSource.setException(e);
+                });
+
+        return taskCompletionSource.getTask();
+    }
 }
